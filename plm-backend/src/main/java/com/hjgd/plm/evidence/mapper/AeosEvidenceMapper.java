825cec36b8d0a416cb7baeedf635922fd94a7708
futianyu760118-art <futianyu760118@gmail.com>
2026-09-23 13:44:18 +0800

    AEOS RD V0.2: add plm-backend/src/main/java/com/hjgd/plm/evidence/mapper/AeosEvidenceMapper.java

---
+++ plm-backend/src/main/java/com/hjgd/plm/evidence/mapper/AeosEvidenceMapper.java
@@ -0,0 +1,9 @@
+package com.hjgd.plm.evidence.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hjgd.plm.evidence.entity.AeosEvidence;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AeosEvidenceMapper extends BaseMapper<AeosEvidence> {
+}
