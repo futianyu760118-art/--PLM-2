d58e303c76a9ad32c7d08daa900f5557e7f653d7
futianyu760118-art <futianyu760118@gmail.com>
2026-09-23 13:44:07 +0800

    AEOS RD V0.2: add plm-backend/src/main/java/com/hjgd/plm/project/mapper/ProjectNodeApprovalMapper.java

---
+++ plm-backend/src/main/java/com/hjgd/plm/project/mapper/ProjectNodeApprovalMapper.java
@@ -0,0 +1,9 @@
+package com.hjgd.plm.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hjgd.plm.project.entity.ProjectNodeApproval;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ProjectNodeApprovalMapper extends BaseMapper<ProjectNodeApproval> {
+}
