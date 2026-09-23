6df00559d48c5bb955360c83c5d3e8b9f6ca8006
futianyu760118-art <futianyu760118@gmail.com>
2026-09-23 13:44:10 +0800

    AEOS RD V0.2: add plm-backend/src/main/java/com/hjgd/plm/project/mapper/ProjectChangeMapper.java

---
+++ plm-backend/src/main/java/com/hjgd/plm/project/mapper/ProjectChangeMapper.java
@@ -0,0 +1,9 @@
+package com.hjgd.plm.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hjgd.plm.project.entity.ProjectChange;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ProjectChangeMapper extends BaseMapper<ProjectChange> {
+}
